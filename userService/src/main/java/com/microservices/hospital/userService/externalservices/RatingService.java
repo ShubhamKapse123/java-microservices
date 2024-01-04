d81efaad4480e48dfdef5d2899ab950a7a7f4fab
shubham <skapse55555@gmail.com>
2024-01-04 12:08:21 +0530

    add api gateway

--- userService/src/main/java/com/microservices/hospital/userService/externalservices/RatingService.java
+++ userService/src/main/java/com/microservices/hospital/userService/externalservices/RatingService.java
@@ -4,6 +4,7 @@ import com.microservices.hospital.userService.entities.Rating;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -12,5 +13,8 @@ public interface RatingService {
 
     @GetMapping("/rating/users/{userId}")
     Rating[] getRatingList(@PathVariable String userId);
+
+    @PostMapping("/rating")
+    Rating createRating(Rating values);
 
 }
